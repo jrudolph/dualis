38e9727f2fc6a3cf646f79f0f8b4153abdefe6bb
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-08-14 08:10:49 +0200

    some fleshing out, a simple repl

--- src/main/scala/net/virtualvoid/Dualis.scala
+++ src/main/scala/net/virtualvoid/Dualis.scala
@@ -1,5 +1,7 @@
 package net.virtualvoid.dualis
 
+import scala.collection.mutable._
+
 /**
  * A tool for analysis of binary representations
  */
@@ -10,17 +12,27 @@ trait Type {
   //def valueToString(
 }
 
+abstract class PrimitiveType(val name: String) extends Type
+
+object Primitives {
+  case object Byte extends PrimitiveType("Byte")
+}
+
 /**
  * A sequence of values read sequentially from the input stream
  */
-trait SequentialType extends Type
+trait Struct extends Type {
+  def members: List[(String, Type)]
+}
 
 /**
  * A constant magic number expected at some position
  */
-trait MagicNumber extends Type
+trait MagicNumber extends Type {
+  def bytes: Array[Byte]
+}
 
-trait InstanceArray {
+trait InstanceArray extends Type {
   def elementType: Type
 }
 
@@ -39,9 +51,11 @@ trait DynamicArray extends InstanceArray {
 }
 
 /**
- * A position inside a bytestream
+ * A position inside a bytearray
  */
-trait Position
+trait Position {
+  
+}
 
 /**
  * A range inside a bytestream
@@ -59,9 +73,80 @@ trait Instance {
 // List[ByteRange => Instance]
 trait Covering
 
+trait ByteArray {
+  def bytesAt(offset: Long, length: Long): Array[Byte]
+  def size: Long
+}
+
+package impl {
+  import net.virtualvoid.dualis
+
+  abstract class MutableType(var name: String = "unnamed") extends Type
+  class MutableStruct(name: String) extends MutableType(name) with dualis.Struct {
+    private val _members = new LinkedHashMap[String, Type]
+    def add(member: (String, Type)): Unit = _members += member
+    def remove(name: String): Unit = _members remove name   
+    
+    def find(name: String): Option[Type] = _members get name
+    def members = _members.toList
+  }
+  class FixedArray(var elementType: Type, var length: Long) extends MutableType with dualis.FixedArray
+}
 
 object Dualis {
+  val Exit = "exit|\u0004".r
+
+  import java.io._
+  val reader = new BufferedReader(new InputStreamReader(System.in))
+
+  def readLine = reader.readLine
+
+  def dispatch(cmd: String) = cmd match {
+    case Exit() | null => exit(0)
+    case _ => println("Command not found: '"+cmd+"'")
+  }
+
+  def repl(file: ByteArray) {
+    //val root = new impl.MutableStruct("unnamed")
+    //root.add("bytes" -> new impl.FixedArray(Primitives.Byte, file.size))
+
+    while(true) {
+      print("> ")
+      val cmd = readLine
+
+      dispatch(cmd)
+    }
+  }
   def main(args: Array[String]) {
     println("Hello")
+    repl(null)
   }
 }
+
+/*
+ Sample session:
+
+> load abc.class
+Loaded 'abc.class', 125 Bytes
+
+root = unnamed Sequential[125] {
+ bytes[125] = [ca fe ba be 00 05 12 ...]
+}
+> name ClassFile
+
+root = ClassFile Sequential[125] {
+ bytes[125] = [ca fe ba be 00 05 12 ...]
+}
+> magic Magic 0xcafebabe
+
+root = ClassFile Sequential[125] {
+ Magic[ca fe ba be]
+ bytes[121] = [00 05 12 a0 ...]
+}
+> seq Version version
+
+Created new type Version
+root.version =  
+
+
+*/
