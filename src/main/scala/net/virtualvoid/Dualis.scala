ef83337e6200e9a7aa498fe2ee14b9e4046824d7
Johannes Rudolph <johannes_rudolph@gmx.de>
2010-08-15 11:54:44 +0200

    * use ByteRanges as the primary means to access to data  * changed parsing to work on ByteRange and return rest ByteRange  * ConsumeAllBytes which replaces the FixedArray in a mutable Struct

--- src/main/scala/net/virtualvoid/Dualis.scala
+++ src/main/scala/net/virtualvoid/Dualis.scala
@@ -16,7 +16,7 @@ trait ByteArray {
 trait Type {
   def name: String
 
-  def parse(buffer: ByteArray, pos: PositionType): Instance
+  def parse(range: ByteRange): (ByteRange, Instance)
   def repr(instance: Instance): String
   //def length: Option[Long]
   //def valueToString(
@@ -28,7 +28,7 @@ trait Type {
 trait Struct extends Type {
   def members: List[(String, Type)]
   def find(name: String): Option[Type]
-  override def parse(array: ByteArray, pos: PositionType): CompositeInstance
+  override def parse(range: ByteRange): (ByteRange, CompositeInstance)
 }
 
 /**
@@ -40,7 +40,7 @@ trait MagicNumber extends Type {
 
 trait InstanceArray extends Type {
   def elementType: Type
-  override def parse(array: ByteArray, pos: PositionType): InstanceCollection
+  override def parse(range: ByteRange): (ByteRange, InstanceCollection)
 }
 
 /**
@@ -70,7 +70,11 @@ trait Position {
 trait ByteRange {
   def data: ByteBuffer
   def dataBytes: Array[Byte]
-  def positionAfter: PositionType
+  def length: Int
+  def consume(bytes: Int): (ByteRange, ByteRange)
+  def -(other: ByteRange): ByteRange
+  def at(pos: PositionType, length: Int): ByteRange
+  //def positionAfter: PositionType
 }
 
 /*
@@ -99,7 +103,17 @@ trait Covering
 
 package impl {
   import net.virtualvoid.dualis
-
+  
+  object ByteRange {
+    object Empty extends dualis.ByteRange {
+      override def data = ByteBuffer.allocate(0)
+      override def dataBytes = Array()
+      override def consume(bytes: Int) = throw new RuntimeException("No bytes left")
+      override def -(other: dualis.ByteRange): dualis.ByteRange = throw new RuntimeException("No bytes left")
+      override def length = 0
+      override def at(pos: PositionType, length: Int): dualis.ByteRange = throw new RuntimeException("No bytes left")
+    }
+  }
   case class ByteRange(val array: ByteArray, val offset: PositionType, val length: Int) extends dualis.ByteRange {
     def data = array.bytesAt(offset, length)
     def positionAfter = offset + length
@@ -108,13 +122,26 @@ package impl {
       data.get(buffer)
       buffer
     }
+    override def consume(bytes: Int): (dualis.ByteRange, dualis.ByteRange) =
+      (ByteRange(array, offset, bytes), ByteRange(array, offset + bytes, length - bytes))
+    override def -(other2: dualis.ByteRange): dualis.ByteRange = {
+      //val other = other2.asInstanceOf[ByteRange]
+      //assert(array == other.array)
+      //assert(other.offset > offset)
+      ByteRange(array, offset, length - other2.length)
+    }
+    override def at(pos: PositionType, length: Int): dualis.ByteRange = {
+      ByteRange(array, offset + pos, length)
+    }
   }
 
   case class SimpleInstance(val tpe: Type, val range: dualis.ByteRange) extends Instance
 
   abstract class PrimitiveType(val name: String, bytes: Int) extends Type {
-    override def parse(buffer: ByteArray, pos: PositionType): Instance =
-      SimpleInstance(this, ByteRange(buffer, pos, bytes))
+    override def parse(range: dualis.ByteRange): (dualis.ByteRange, Instance) = {
+      val (myRange, nextRange) = range.consume(bytes)
+      (nextRange, SimpleInstance(this, myRange))
+    }
   }
 
   object Primitives {
@@ -127,55 +154,58 @@ package impl {
        extends dualis.CompositeInstance
 
   trait StructImpl extends Struct {
-    override def parse(buffer: ByteArray, pos: PositionType): CompositeInstance = {
-      case class State(instances: List[(String, Instance)], pos: PositionType)
+    override def parse(range: dualis.ByteRange): (dualis.ByteRange, CompositeInstance) = {
+      case class State(instances: List[(String, Instance)], range: dualis.ByteRange)
       
       val State(memberInstances, afterLast) = 
-        members.foldLeft(State(Nil, pos)) { (state, member) =>
+        members.foldLeft(State(Nil, range)) { (state, member) =>
           (state, member) match {
             case (State(rest, pos), (name, tpe)) =>
-              val next = tpe.parse(buffer, state.pos)
-            State((name, next)::state.instances, next.range.positionAfter)
+              val (nextRange, next) = tpe.parse(state.range)
+              State((name, next)::state.instances, nextRange)
           }
         }
       
-      CompositeInstance(this, ByteRange(buffer, pos, afterLast - pos), memberInstances)
+      (afterLast, CompositeInstance(this, range - afterLast, memberInstances.reverse))
     }
 
     override def repr(i: Instance): String = {
       val memberReprs =
         i.asInstanceOf[CompositeInstance].namedMembers map { case (name, i) => name+": "+find(name).get.name+" = "+i.repr}
       
-      "{\n"+memberReprs.mkString("\t","","\n")+"}"
+      "{\n"+memberReprs.mkString("\t","\n\t","\n")+"}"
     }
   }
 
   case class InstanceCollection(val tpe: Type, val range: dualis.ByteRange, val members: List[Instance])
     extends dualis.InstanceCollection
 
-  trait FixedArrayImpl extends dualis.FixedArray {
-    override def parse(buffer: ByteArray, pos: PositionType): dualis.InstanceCollection = {
-      // Factor out with StructImpl
-      case class State(instances: List[Instance], pos: PositionType)
-      
-      val State(memberInstances, afterLast) = 
-        (0L until length).foldLeft(State(Nil, pos)) { (state, member) =>
-          state match {
-            case State(rest, pos) =>
-              val next = elementType.parse(buffer, state.pos)
-              State(next::state.instances, next.range.positionAfter)
-          }
-        }
-      
-      InstanceCollection(this, ByteRange(buffer, pos, afterLast - pos), memberInstances.reverse)
-    }
-
+  trait InstanceArrayImpl extends dualis.InstanceArray {
     override def repr(i: Instance): String = {
       val MAX = 5
       val membs = i.asInstanceOf[InstanceCollection].members
       val memrep = membs take MAX map (_.repr)
       "["+memrep.mkString(", ")+(if (membs.size > MAX) ", ..." else "")+"]"
+    }    
+  }
+
+  trait FixedArrayImpl extends dualis.FixedArray with InstanceArrayImpl {
+    override def parse(range: dualis.ByteRange): (dualis.ByteRange, dualis.InstanceCollection) = {
+      // Factor out with StructImpl
+      case class State(instances: List[Instance], range: dualis.ByteRange)
+      
+      val State(memberInstances, afterLast) = 
+        (0L until length).foldLeft(State(Nil, range)) { (state, member) =>
+          state match {
+            case State(rest, pos) =>
+              val (nextRange, next) = elementType.parse(state.range)
+              State(next::state.instances, nextRange)
+          }
+        }
+      
+      (afterLast, InstanceCollection(this, range - afterLast, memberInstances.reverse))
     }
+    
     override def name: String = elementType.name+"["+length+"]"
   }
 
@@ -185,11 +215,20 @@ package impl {
     def add(member: (String, Type)): Unit = _members += member
     def remove(name: String): Unit = _members remove name   
     
-    def find(name: String): Option[Type] = _members get name
-    override def members = _members.toList
+    def find(name: String): Option[Type] = if (name == "free") Some(ConsumeAllBytes) else _members get name
+    override def members = _members.toList :+ ("free" -> ConsumeAllBytes)
   }
   class MutableFixedArray(var elementType: Type, var length: Long) 
     extends Type with dualis.FixedArray with impl.FixedArrayImpl
+
+  case object ConsumeAllBytes extends InstanceArrayImpl {
+    override def elementType = Primitives.Byte
+    override def parse(range: dualis.ByteRange): (dualis.ByteRange, InstanceCollection) = {
+      val members = range.dataBytes.zipWithIndex map { case (b, i) => SimpleInstance(Primitives.Byte, range.at(i, 1)) }
+      (ByteRange.Empty, InstanceCollection(this, range, members.toList))
+    }
+    override def name = "Rest"
+  }
 
   import java.io._
   class FileByteArray(f: File) extends ByteArray {
@@ -200,6 +239,8 @@ package impl {
 
     override def size = f.length
     override def bytesAt(pos: Int, length: Int) = {
+      if (pos + length > raFile.length)
+        throw new RuntimeException("Tried to read beyond file borders filesize = %d pos = %d length = %d".format(raFile.length, pos, length))
       val buffer = ByteBuffer.allocate(length)
       val read = channel.read(buffer, pos)
       buffer.position(0)
@@ -209,7 +250,21 @@ package impl {
 }
 
 object Dualis {
-  val Exit = "exit|\u0004".r
+  object Commands {
+    object TypeNameOf {
+      val Types =
+        Map("b" -> impl.Primitives.Byte)
+      def unapply(name: String): Option[Type] = Types.get(name)
+    }
+    object CreateNamedElement {
+      val Create = "(\\w*) (\\w*)".r
+      def unapply(cmd: String): Option[(Type, String)] = cmd match {
+        case Create(TypeNameOf(tpe), memberName) =>
+          Some(tpe, memberName)
+        case _ => None
+      }
+    }
+  }
 
   import java.io._
   val reader = new BufferedReader(new InputStreamReader(System.in))
@@ -218,16 +273,22 @@ object Dualis {
 
   def repl(file: ByteArray) {
     val root = new impl.MutableStruct("unnamed")
-    root.add("bytes" -> new impl.MutableFixedArray(impl.Primitives.Byte, file.size))
 
-    var instance = root.parse(file, 0)
-
+    val Exit = "exit|\u0004".r
+    val Rename = "name (\\w*)".r
+    import Commands._
     def dispatch(cmd: String) = cmd match {
       case Exit() | null => exit(0)
+      case Rename(name) => root.name = name
+      case CreateNamedElement(tpe, name) => 
+        println("Creating "+name+" of type "+tpe)
+        root.add(name -> tpe)
       case _ => println("Command not found: '"+cmd+"'")
     }
 
     while(true) {
+      val (_, instance) = root.parse(impl.ByteRange(file, 0, file.size.toInt))
+
       println("root: "+root.name+" = "+instance.repr+"\n")
       print("> ")
       val cmd = readLine
